9019e0224960f81efe5f2c7fa2e9f81138fe41a9
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-02-01 12:10:42 +0100

    drop page table, drop page controller, create static page model, validation and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_184514) do
+ActiveRecord::Schema.define(version: 2020_02_01_105926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,14 +67,14 @@ ActiveRecord::Schema.define(version: 2020_01_30_184514) do
     t.index ["user_id"], name: "index_newsletters_on_user_id"
   end
 
-  create_table "pages", force: :cascade do |t|
+  create_table "static_pages", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.bigint "user_id"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_pages_on_user_id"
+    t.index ["user_id"], name: "index_static_pages_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -104,5 +104,5 @@ ActiveRecord::Schema.define(version: 2020_01_30_184514) do
   add_foreign_key "newsletter_articles", "articles"
   add_foreign_key "newsletter_articles", "newsletters"
   add_foreign_key "newsletters", "users"
-  add_foreign_key "pages", "users"
+  add_foreign_key "static_pages", "users"
 end
