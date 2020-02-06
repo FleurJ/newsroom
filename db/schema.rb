c8b578f7cfa9c89c3590c406770a7d13912afa29
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-02-06 17:05:26 +0100

    add defaut role to new user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_125809) do
+ActiveRecord::Schema.define(version: 2020_02_06_160059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2020_02_01_125809) do
     t.string "first_name"
     t.string "agora_profil"
     t.string "status"
-    t.string "role"
+    t.string "role", default: "user"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
