8255ea91cad93802cd553699fed2aa931336bda7
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-02-11 19:58:11 +0100

    add press review date and update index view and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_160059) do
+ActiveRecord::Schema.define(version: 2020_02_11_175918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_02_06_160059) do
     t.text "teaser"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "press_review_date"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = User.create!([{ email: "admin1@email.com", password: "123456", role: "admin"},
-{ email: "user1@email.com", password: "654321", role: "user"}, { email: "editor1@email.com", password: "123456", role: "editor"}])
 
-tags = Tag.create!([{name: "Tag 1", status: "published"}, {name: "Tag 1", status: "draft"}])
-
-articles = Article.create!([{title: "Titre d'article", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et purus vel odio posuere dictum nec non justo. Maecenas posuere leo dolor, pharetra faucibus odio viverra sagittis. Aenean nec elit odio. Nulla non orci velit. Nulla facilisi. Aenean tempor turpis vitae enim porta, a egestas libero fermentum. Pellentesque volutpat nec ligula vel fringilla. Fusce sed ante non ligula venenatis dapibus. Sed odio leo, auctor id ornare vitae, mollis dictum augue. Sed sed interdum elit, quis viverra lorem.", status: "published", source_name: "source", user_id: "1"},
-{title: "Article draft", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et purus vel odio posuere dictum nec non justo. Maecenas posuere leo dolor, pharetra faucibus odio viverra sagittis. Aenean nec elit odio. Nulla non orci velit. Nulla facilisi. Aenean tempor turpis vitae enim porta, a egestas libero fermentum. Pellentesque volutpat nec ligula vel fringilla. Fusce sed ante non ligula venenatis dapibus. Sed odio leo, auctor id ornare vitae, mollis dictum augue. Sed sed interdum elit, quis viverra lorem.", status: "draft", source_name: "source 2", user_id: "1"}])
