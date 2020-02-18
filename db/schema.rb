c173de35eb02de6bfb5afa3be4fbed478db15751
Fleur Jasoigne <fleur.jasoigne@ares-ac.be>
2020-02-18 19:37:50 +0100

    navtabs avec condition

--- db/schema.rb
+++ db/schema.rb
@@ -91,12 +91,11 @@ ActiveRecord::Schema.define(version: 2020_02_15_151850) do
 
   create_table "newsletters", force: :cascade do |t|
     t.string "title"
-    t.string "newsletter_type"
+    t.string "type"
     t.bigint "user_id"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "press_review_date"
     t.index ["user_id"], name: "index_newsletters_on_user_id"
   end
 
@@ -130,7 +129,6 @@ ActiveRecord::Schema.define(version: 2020_02_15_151850) do
     t.string "agora_profil"
     t.string "status"
     t.string "role", default: "user"
-    t.string "subscribed"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
