e62001e5c7b2ae826eacc61f24ddee233e35a098
Fleur Jasoigne <fleur.jasoigne@ares-ac.be>
2020-02-22 11:34:47 +0100

    functionnal savedraft & savepublished buttons + css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,3 @@
 import "bootstrap";
+import { saveAsDraft } from "./button.js";
+import { saveAndPublish } from "./button.js";
--- app/javascript/packs/button.js
+++ app/javascript/packs/button.js
@@ -1,16 +1,18 @@
-const saveAsDraft = document.querySelector("#save-draft");
+const saveAsDraft = document.querySelector("#save_draft");
+
 
 saveAsDraft.addEventListener("click", event => {
   console.log("Element clicked");
-  let article_status = document.getElementById("status");
+  let article_status = document.getElementById("article_status");
   article_status.value = 'draft';
 });
 
 
 const saveAndPublish = document.querySelector("#save_published");
-
 saveAndPublish.addEventListener("click", event => {
-  console.log("Element clicked");
-  let article_status = document.getElementById("status");
+  console.log("Element publish clicked");
+  let article_status = document.getElementById("article_status");
   article_status.value = 'published';
 });
+
+export {saveAsDraft, saveAndPublish};
