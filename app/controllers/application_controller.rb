5d2c272970e2e47d2281425cea47b82fd25e842f
Fleur Jasoigne <fleur.jasoigne@ares-ac.be>
2020-02-28 20:11:49 +0100

    meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-  stored_location_for(resource) || root_path
+    stored_location_for(resource) || root_path
   end
 
+  def default_url_options
+    { host: ENV["kiosk-ares.herokuapp.com"] || "localhost:3000" }
+  end
 end
